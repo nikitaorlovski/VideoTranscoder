22fc27274075b712fe1c85cc7e7e653959e84990
Nikita Orlovsky <orlovski15555@gmail.com>
2026-02-19 20:09:29 +0300

    add users table

---
+++ app/models/users.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Integer, String, LargeBinary, Boolean
+from sqlalchemy.orm import mapped_column, Mapped
+from app.database.db import Base
+
+class UserOrm(Base):
+    __tablename__ = 'users'
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    email: Mapped[str] = mapped_column(String, unique=True)
+    username: Mapped[str] = mapped_column(String)
+    password: Mapped[bytes] = mapped_column(LargeBinary)
+    is_admin: Mapped[bool] = mapped_column(Boolean)
