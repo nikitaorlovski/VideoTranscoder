fd745370adf463bdaf1b20393e240c9ca97dd048
Nikita Orlovsky <orlovski15555@gmail.com>
2026-02-20 18:29:27 +0300

    add access_token_expire and refactoring with black

--- app/core/config.py
+++ app/core/config.py
@@ -6,6 +6,7 @@ from sqlalchemy import URL
 
 ROOT = Path(__file__).resolve().parent.parent.parent
 
+
 class DbSettings(BaseModel):
     host: str
     port: int
@@ -15,25 +16,43 @@ class DbSettings(BaseModel):
 
     @property
     def url(self):
-        return URL.create(drivername="postgresql+asyncpg", database=self.name, host=self.host,port=self.port,username=self.username,password=self.password)
+        return URL.create(
+            drivername="postgresql+asyncpg",
+            database=self.name,
+            host=self.host,
+            port=self.port,
+            username=self.username,
+            password=self.password,
+        )
 
     @property
     def sqlalchemy_url(self):
-        return URL.create(drivername="postgresql+asyncpg", database=self.name, host="localhost",port=self.port,username=self.username,password=self.password)
+        return URL.create(
+            drivername="postgresql+asyncpg",
+            database=self.name,
+            host="localhost",
+            port=self.port,
+            username=self.username,
+            password=self.password,
+        )
+
 
 class AuthSettings(BaseModel):
     algorithm: str = "RS256"
-    private_key_path: Path = ROOT / "certs" /  "private.pem"
-    public_key_path: Path = ROOT / "certs" /  "public.pem"
+    private_key_path: Path = ROOT / "certs" / "private.pem"
+    public_key_path: Path = ROOT / "certs" / "public.pem"
+    access_token_expire_minutes: int = 60
+
 
 class Settings(BaseSettings):
     db: DbSettings
     auth: AuthSettings = AuthSettings()
 
     model_config = SettingsConfigDict(
-        env_file= str(ROOT / ".env"),
+        env_file=str(ROOT / ".env"),
         env_file_encoding="utf-8",
-        env_nested_delimiter="__"
+        env_nested_delimiter="__",
     )
+
 
 settings = Settings()
