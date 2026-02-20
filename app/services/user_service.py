609079414cd2230e4bb1b7bc2d7225b0f53ee69a
Nikita Orlovsky <orlovski15555@gmail.com>
2026-02-20 22:06:30 +0300

    add user service and business-logic

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -1,6 +1,32 @@
-from app.schemas import UserRegister
+from app.domain.exceptions import UserAlreadyExists
+from app.models.users import UserOrm
+from app.repositories.user_repository import UserRepository
+from app.schemas.users import UserRegister, UserSchema
+from app.core.security import hash_password, encode_jwt
+
+from app.schemas.token import Token
 
 
 class UserService:
-    async def register(self, user: UserRegister):
-        pass
+    def __init__(self, repo: UserRepository):
+        self.repo = repo
+
+    async def register(self, user: UserRegister) -> Token:
+        if exists_user := await self.repo.get_user_by_email(user.email):
+            raise UserAlreadyExists()
+        hashed_password = hash_password(user.password)
+        new_user = await self.repo.register(
+            UserOrm(
+                username=user.username,
+                email=user.email,
+                password=hashed_password,
+            )
+        )
+        payload = {"sub": str(new_user.id), "email": new_user.email}
+        user_jwt = encode_jwt(payload)
+        return Token(access_token=user_jwt, token_type="bearer")
+
+    async def get_user_by_email(self, email: str) -> UserSchema | None:
+        if not (user := await self.repo.get_user_by_email(email)):
+            return None
+        return UserSchema.model_validate(user)
