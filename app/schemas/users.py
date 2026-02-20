01e833ec18caba176b69de45e087368ab5b80aa3
Nikita Orlovsky <orlovski15555@gmail.com>
2026-02-20 22:04:25 +0300

    add schemas with all data for user

--- app/schemas/users.py
+++ app/schemas/users.py
@@ -1,6 +1,14 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, ConfigDict
 
 class UserRegister(BaseModel):
     username: str
     email: EmailStr
     password: str
+
+class UserSchema(BaseModel):
+    id: int
+    email: EmailStr
+    username: str
+    password: bytes
+    is_admin: bool
+    model_config = ConfigDict(from_attributes=True)
