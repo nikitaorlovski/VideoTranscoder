90b0b87d3fd27c6920046efff2e0ac5c2a6d5764
Nikita Orlovsky <orlovski15555@gmail.com>
2026-02-20 18:23:17 +0300

    add methods for jwt encode/decode

--- app/core/security.py
+++ app/core/security.py
@@ -1,8 +1,29 @@
 import bcrypt
+import jwt
+
+from app.core.config import settings
+
 
 def hash_password(password: str) -> bytes:
     salt = bcrypt.gensalt()
     return bcrypt.hashpw(password.encode(), salt)
 
+
 def check_password(password: str, hashed_password: bytes) -> bool:
     return bcrypt.checkpw(password.encode(), hashed_password)
+
+
+def encode_jwt(
+    payload: dict,
+    algorithm: str = settings.auth.algorithm,
+    private_key: str = settings.auth.private_key_path.read_text(),
+) -> str:
+    return jwt.encode(payload=payload, algorithm=algorithm, key=private_key)
+
+
+def decode_jwt(
+    token: str,
+    algorithm: str = settings.auth.algorithm,
+    public_key: str = settings.auth.public_key_path.read_text(),
+) -> str:
+    return jwt.decode(jwt=token, key=public_key, algorithms=[algorithm])
