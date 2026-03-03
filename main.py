5d3bbaf938623ca1c50a2dcf08790f6f9f722642
Nikita Orlovsky <orlovski15555@gmail.com>
2026-03-03 15:40:55 +0300

    add lifespan

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 import uvicorn
+from redis import Redis
 from fastapi import FastAPI
-from app.api.v1 import auth_router
+from app.api.v1 import auth_router, video_router
+from contextlib import asynccontextmanager
 
-app = FastAPI()
+from app.core.config import settings
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    redis = Redis(
+        host=settings.redis.host, port=settings.redis.port, db=settings.redis.db
+    )
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
 app.include_router(auth_router)
+app.include_router(video_router)
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
