43455cc0be81bc033cda82ec0097fe5199a2504c
Nikita Orlovsky <orlovski15555@gmail.com>
2026-03-11 16:14:29 +0300

    add response-schema for convert video

--- app/schemas/videos.py
+++ app/schemas/videos.py
@@ -1,8 +1,13 @@
 from pydantic import BaseModel
 
-
 class VideoMeta(BaseModel):
     owner_id: int
     filename: str
     extension: str
     size: int
+
+
+class VideoConvertResponse(BaseModel):
+    video_id: str
+    task_id: str
+    status: str
