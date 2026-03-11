476c96f515b2bd4e374236d6d8ff4e255aabd193
Nikita Orlovsky <orlovski15555@gmail.com>
2026-03-11 17:24:32 +0300

    add task router

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,4 +1,5 @@
 from app.api.v1.auth import router as auth_router
 from app.api.v1.video import router as video_router
+from app.api.v1.task import router as task_router
 
-__all__ = ["auth_router", "video_router"]
+__all__ = ["auth_router", "video_router", "task_router"]
---
+++ app/api/v1/task.py
@@ -0,0 +1,16 @@
+from fastapi import APIRouter
+from celery.result import AsyncResult
+
+router = APIRouter(prefix="/task", tags=["Tasks"])
+
+
+@router.get("/tasks/{task_id}")
+async def get_task_status(task_id: str):
+
+    task_result = AsyncResult(task_id)
+
+    return {
+        "task_id": task_id,
+        "status": task_result.status,
+        "result": task_result.result if task_result.successful() else None,
+    }
