16e6e786782589dfe94925284ca232c612cfb88f
Nikita Orlovsky <orlovski15555@gmail.com>
2026-03-11 16:14:57 +0300

    add task for convert video in celery

--- app/celery/celery_app.py
+++ app/celery/celery_app.py
@@ -3,3 +3,4 @@ from celery import Celery
 from app.core.config import settings
 
 app = Celery(broker=settings.redis.url, backend=settings.redis.url)
+app.conf.imports = ("app.celery.tasks.video_tasks",)
--- app/celery/tasks/video_tasks.py
+++ app/celery/tasks/video_tasks.py
@@ -1,9 +1,19 @@
+import asyncio
+
 from app.celery.celery_app import app as celery_app
-from app.schemas.videos import Video
+from app.repositories.video_repository import VideoRepository
+from app.services.video_service import VideoService
+from app.database.db import new_session
 
 
-@celery_app.task(name="save_video")
-async def save_video(video: Video):
-    pass
-    # TODO
-    # Делать через сервисы и т.д как ранее
+@celery_app.task(name="convert_to_480")
+def convert_to_480_task(video_id: str):
+    asyncio.run(_convert_to_480(video_id))
+
+
+async def _convert_to_480(video_id: str):
+
+    async with new_session() as session:
+        repo = VideoRepository(session)
+        service = VideoService(repo)
+        await service.convert_to_480(video_id)
