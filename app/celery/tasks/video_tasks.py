84514e3e1d63858591d2566978bbe6e2b533b866
Nikita Orlovsky <orlovski15555@gmail.com>
2026-03-11 17:25:18 +0300

    add retry policy

--- app/celery/tasks/video_tasks.py
+++ app/celery/tasks/video_tasks.py
@@ -6,8 +6,16 @@ from app.services.video_service import VideoService
 from app.database.db import new_session
 
 
-@celery_app.task(name="convert_to_480")
-def convert_to_480_task(video_id: str):
+@celery_app.task(
+    name="convert_to_480",
+    bind=True,
+    autoretry_for=(RuntimeError,),
+    retry_backoff=True,
+    retry_backoff_max=60,
+    retry_jitter=True,
+    retry_kwargs={"max_retries": 3},
+)
+def convert_to_480_task(self, video_id: str):
     asyncio.run(_convert_to_480(video_id))
 
 
